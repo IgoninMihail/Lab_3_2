dca753d272c41a5215e8c1d5aa4b68d77ace52ed
IgoninMihail <92269370+IgoninMihail@users.noreply.github.com>
2021-12-05 13:15:08 +0200

    Добавьте файлы проекта.

---
+++ Lab_3_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Ввести число N:");
+                int N = int.Parse(Console.ReadLine());
+
+                for (int a = 1; a <= N; a++ )
+                {
+                    for(int b = 1; b <= N; b++)
+                    {
+                        int c = (a + b) * (a - b);
+                        Console.WriteLine($"c:={c}");
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
